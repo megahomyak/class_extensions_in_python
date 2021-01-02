8004a70c5b0c9c946ca8c5ed493106233a97e264
megahomyak <g.megahomyak@gmail.com>
2021-01-02 22:40:52 +0500

    Moved tests to tests/

---
+++ using_init/tests/usage.py
@@ -0,0 +1,4 @@
+from using_init.tests import base_class
+
+a = base_class.A("using_init.tests.extension_class")
+a.a()
--- using_init/usage.py
+++
@@ -1,4 +0,0 @@
-from using_init import base_class
-
-a = base_class.A("using_init.extensions_class")
-a.a()
