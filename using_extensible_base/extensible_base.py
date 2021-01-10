4816f6548b712b57272d8e610a8cf33980d76404
megahomyak <g.megahomyak@gmail.com>
2021-01-10 18:07:48 +0500

    Why the fuck I left __init__ here

--- using_extensible_base/extensible_base.py
+++ using_extensible_base/extensible_base.py
@@ -1,4 +1,3 @@
-import functools
 import inspect
 import itertools
 from copy import deepcopy
@@ -11,58 +10,6 @@ from using_extensible_base.exceptions import (
 
 
 class ExtensibleBase:
-
-    def __init__(
-            self, extension_file_name: str, class_name: Optional[str] = None):
-        # Getting extension class by name of the current class from imported
-        # file
-        if class_name is None:
-            class_name = self.__class__.__name__
-        try:
-            extension_class: object = __import__(
-                extension_file_name, fromlist=[class_name]
-            ).__dict__[class_name]
-        except KeyError:
-            raise ExtensionClassNotFoundError(
-                f"Nothing named '{class_name}' is found in file "
-                f"'{extension_file_name}', which should contain the extension "
-                f"class named '{class_name}'"
-            )
-        if not inspect.isclass(extension_class):
-            raise ExtensionClassNotFoundError(
-                f"'{class_name}' from '{extension_file_name}' has "
-                f"type {type(extension_class)}, but it should be a class! "
-                f"(type {type(type)})"  # Python...
-            )
-        for field_name, field in extension_class.__dict__.items():
-            try:
-                if (
-                    callable(field) or callable(field.__get__)
-                    and field_name not in ("__dict__", "__weakref__")
-                ):
-                    # Using self.__class__.__dict__ cuz self.__dict__ is empty
-                    if field_name not in [
-                        self.__class__.__dict__,
-                        *[
-                            superclass.__dict__
-                            for superclass in self.__class__.__mro__
-                        ]  # Jewish tricks
-                    ]:
-                        raise MethodNotFoundInBaseClassError(
-                            f"Method '{field_name}' isn't in the base class!"
-                        )
-                    if callable(field):
-                        pure_method = field
-                    else:
-                        pure_method = field.__get__
-                    # Using functools.partial because self isn't being passed to
-                    # methods, because they are methods from the class, not from
-                    # instance of a class
-                    self.__dict__[field_name] = functools.partial(
-                        pure_method, self
-                    )
-            except AttributeError:
-                pass
 
     def __new__(
             cls, extension_file_name: str,
@@ -94,6 +41,9 @@ class ExtensibleBase:
             # Jewish tricks
         }
         print(all_accessible_fields)
+        abstract_methods = list(cls_copy.__dict__.get(
+            "__abstractmethods__", ()
+        ))
         for field_name, field in extension_class.__dict__.items():
             try:
                 if (
@@ -114,8 +64,18 @@ class ExtensibleBase:
                     # methods, because they are methods from the class, not from
                     # instance of a class
                     setattr(cls_copy, field_name, pure_method)
+                    try:
+                        abstract_methods.remove(field_name)
+                    except ValueError:
+                        pass
             except AttributeError:
                 pass
+        if abstract_methods:
+            setattr(
+                cls_copy, "__abstractmethods__", frozenset(abstract_methods)
+            )
+        else:
+            delattr(cls_copy, "__abstractmethods__")
         # noinspection PyArgumentList
         print("fdfosjds", cls_copy.__dict__)
         return super().__new__(cls_copy)
